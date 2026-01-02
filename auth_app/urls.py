4fb3790d3887c12653c2cd033ce4f41eb9a7faea
abndaraa199810-coder <abndaraa199810@gmail.com>
2026-01-02 07:45:34 +0300

    Update urls.py

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # صفحات
+   
     path("", views.dashboard_page, name="dashboard"),
     path("dashboard/", views.dashboard_page, name="dashboard_page"),
     path("enroll-page/", views.enroll_page, name="enroll_page"),
@@ -11,12 +11,10 @@ urlpatterns = [
     path("attendance/", views.attendance_page, name="attendance_page"),
     path("attendance-result/", views.attendance_result, name="attendance_result"),
 
-    # APIs (مهم: المسارات اللي تستخدمها صفحات HTML)
-    path("auth/enroll-face/", views.enroll_face, name="enroll_face"),  # ✅ هنا التعديل
+    path("auth/enroll-face/", views.enroll_face, name="enroll_face"),   
     path("auth/verify/", views.verify, name="verify"),
     path("auth/attendance/", views.attendance_api, name="attendance_api"),
 
-    # Auth API (لو تستخدمينها)
     path("api/register/", views.RegisterView.as_view(), name="register"),
     path("api/login/", views.LoginView.as_view(), name="login"),
 ]
